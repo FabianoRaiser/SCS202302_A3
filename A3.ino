6b2c87e9eeaff04e72c9649136981d7ee2536273
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-12-04 19:11:55 -0300

    :wrench: refactor: Separando em Funções separadas e add feedback no serial

--- A3.ino
+++ A3.ino
@@ -1,28 +1,34 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-uint8_t broadcastAddress[] = { 0xD8, 0xBC, 0x38, 0xE2, 0x48, 0x9C };
+// Definir endereço da placa comunicadora
+uint8_t broadcastAddress[] = {0xD8, 0xBC, 0x38, 0xE2, 0x48, 0x9C};
+
+// Buffer de memória
 String buffer[9];
 
-typedef struct struct_message {
+// Difinição do padrão da mensagem recebida
+typedef struct struct_message
+{
+  String req;
   String S;
   String R;
 } struct_message;
-
-int i;
-
 struct_message myData;
 
 esp_now_peer_info_t peerInfo;
 
+int i;
+
 // Reponde quando dados enviados
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
   Serial.print("\r\Status do ultimo pacote enviado:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Successo na entrega" : "Falha na entrega");
 }
 
-
-void setup() {
+void setup()
+{
   // Iniciar Monitor  Serial
   Serial.begin(115200);
   Serial.println();
@@ -32,7 +38,8 @@ void setup() {
   WiFi.mode(WIFI_STA);
 
   // Inicia ESP-NOW
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("Erro ao inicializar ESP-NOW");
     return;
   }
@@ -46,16 +53,19 @@ void setup() {
   peerInfo.encrypt = false;
 
   // Adicionar peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
     Serial.println("Falha ao adicionar peer");
     return;
   }
 }
 
-void loop() {
+void loop()
+{
 }
 
-void RecebeDados(const uint8_t *mac, const uint8_t *incomingData, int len) {
+void RecebeDados(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Byte Recebido:      ");
   Serial.println(len);
@@ -64,39 +74,68 @@ void RecebeDados(const uint8_t *mac, const uint8_t *incomingData, int len) {
   Serial.print("Letra R:     ");
   Serial.println(myData.R);
 
-  // Se o buffer estiver com espaço livre
-  espacoLivre();
-
-  //Se o consumidor solicitar uma letra
-  if (i != 0) {
-    for (int k = 0; k < 9; k++) {
-      if (buffer[k] == myData.S) {
-        Serial.print(buffer[k]);
-        buffer[k] = "";
-        i--;
-        break;
-      }
-    }
+  if (myData.req == "POST")
+  {
+    insereLetra(); // Se o buffer estiver com espaço livre
+  }
+  else if (myData.req == "GET")
+  {
+    retiraLetra(); // Se o consumidor solicitar uma letra
   }
 
-  //Exibe estado buffer
+  // Exibe estado buffer
   exibeBuffer();
 }
 
-void espacoLivre() {
-  if (i < 9) {
-    for (int l = 0; l < 9; l++) {
-      if (buffer[l] == "") {
-        buffer[i] = myData.R;
+void insereLetra()
+{
+  if (i < 9) // Se houver espaço
+  {
+    for (int l = 0; l < 9; l++) // Percorre o buffer
+    {
+      if (buffer[l] == "") // Se posição vazia
+      {
+        buffer[l] = myData.R;
         i++;
+        Serial.println("Produto armazenado na posição ");
+        Serial.print(l);
+        break;
+      }
+      
+    }
+  }
+  else {
+    Serial.println("Sem espaço para armazenar o produto!");
+    }
+}
+
+void retiraLetra()
+{
+  if (i != 0)
+  {
+    for (int k = 0; k < 9; k++)
+    {
+      if (buffer[k] == myData.S)
+      {
+        buffer[k] = "";
+        i--;
+        Serial.println("Produto retirado da posição ");
+        Serial.print(k);
+        // Função de envio do produto;
         break;
       }
     }
   }
+  else {
+    Serial.println("Estoque Vazio!")
+  }
+
 }
 
-void exibeBuffer() {
-  for (int j = 0; j < 9; j++) {
+void exibeBuffer()
+{
+  for (int j = 0; j < 9; j++)
+  {
     Serial.print("Buffer: ");
     Serial.println(buffer[j]);
   }
